43c62fb8d17b23369d17507c8a7540dd74c3c6b4
Joshua Michael <joshua.kale.michael@gmail.com>
2024-06-20 16:21:32 -0500

    update turso config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,9 +4,9 @@ export default {
 	dialect: 'sqlite',
 	driver: 'turso',
 	schema: './db/schema.ts',
-	out: './db/migrations',
-	dbCredentials: {
-		url: process.env.TURSO_DATABASE_URL as string,
-		authToken: process.env.TURSO_AUTH_TOKEN as string
-	}
+	out: './db/migrations'
+	// dbCredentials: {
+	// 	url: process.env.TURSO_DATABASE_URL as string,
+	// 	authToken: process.env.TURSO_AUTH_TOKEN as string
+	// }
 } satisfies Config
